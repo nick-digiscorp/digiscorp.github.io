35cbe38bf57a26ad6fbafbb6ac239f1399a5a07c
nick-digiscorp <78218026+nick-digiscorp@users.noreply.github.com>
2024-08-22 15:14:07 -0400

    Redux of Website

---
+++ scripts4.js
@@ -0,0 +1,12 @@
+// scripts4.js
+
+document.addEventListener('DOMContentLoaded', () => {
+    const navbar = document.querySelector('.navbar');
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 50) {
+            navbar.classList.add('scrolled');
+        } else {
+            navbar.classList.remove('scrolled');
+        }
+    });
+});
